e0032c5631c52d876320bc61b35a63deef55fb23
taiji-nakam <taiji.naka0002@gmail.com>
2024-12-06 06:20:00 +0900

    診断結果取得API /assessmentResult?assessmentId=XXX 追加

--- module/mdlAssessment.py
+++ module/mdlAssessment.py
@@ -6,11 +6,21 @@ from db_control import crud, mymodels
 def do(data):
     # dataから質問回答を取得
     print(data)
+    
     # 診断をDBに登録
     new_assessment_id = crud.insert_assessment()
 
     # 診断回答をDBに登録
 
-    # 診断結果をDBに登録
 
+    # 診断結果をDBに登録
+    categoryAverages = data['categoryAverages']
+    
     return jsonify({'assessment_id': new_assessment_id})
+
+def getResult(assessmentId):
+    # dataから質問回答を取得
+    print('ID:')
+    print(assessmentId)
+    # 診断結果取得
+    return crud.select_assessment_result(assessmentId)
