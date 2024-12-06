3476170f4ea7a7021bf8d4b2cd5319c49ff860a3
taiji-nakam <taiji.naka0002@gmail.com>
2024-12-07 08:58:49 +0900

    エリア情報テーブル追加、mdlHelloにDBサンプルコード、mdlAreaにコメント追加

--- module/mdlArea.py
+++ module/mdlArea.py
@@ -6,8 +6,25 @@ def do(data):
     # dataから基礎情報取得
 
     # 基礎情報をDBに登録
+    ### mdlArea.pyで使う ###
+    # # [db Sample] basic_infoテーブルへInsert
+    # basic_info_to_insert = mymodels.basic_info(
+    #     assessment_id = new_assessment_id,
+    #     age_group = "30",
+    #     country_origin = "USA",
+    #     nearest_station = "Shibuya Station",
+    #     budget_lower_limit = 80000,
+    #     budget_upper_limit = 150000,
+    #     area_fg_smaller = 0,
+    #     area_fg_average = 1,
+    #     area_fg_larger = 1
+    # )
+    # insert_cnt = crud.myinsert(basic_info_to_insert)
+    # message = message + f"[insert to basic_info]row{insert_cnt}\n "
 
     # 診断結果を取得
+#    # [db Sample] assessment_resultデータを取得、基礎情報と合わせてgptにおすすエリアを提案してもらう
+#     result = crud.select_assessment_result(new_assessment_id)
 
     # プロンプト作成
 
@@ -16,11 +33,20 @@ def do(data):
     # 返答の解析
 
     # おすすめエリアをDBに登録
+    ### mdlArea.pyで使う ###
+    # [db Sample] area_resultテーブルへInsert
+    # area_result_to_insert = mymodels.area_result(
+    #     assessment_id = new_assessment_id,
+    #     recommended = "東京 渋谷区",
+    #     note = "おススメエリア：渋谷区の説明文です"
+    # )
+    # insert_cnt = crud.myinsert(area_result_to_insert)
+    # message = message + f"[insert to area_result]row{insert_cnt}\n "
 
     # 結果を構成
     print("4 OK")
 
-    return jsonify({'message': '[dummy]Assessment Result'})
+    return jsonify({'area': '東京都渋谷区XXX丁目'})
 
     # 参考ソース(GPT)
     # data = request.get_json()  # JSONデータを取得
